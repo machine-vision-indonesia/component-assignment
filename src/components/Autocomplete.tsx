4763f4e7d32d03778fae88191dc3d0d8b400777c
Muhammad Taufiq Febrianto <mtaufiqfebrianto6@gmail.com>
2024-03-15 19:57:50 +0700

    tes

--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,5 +1,5 @@
 import Typography from "@mui/material/Typography";
 
 export function Autocomplete() {
-  return <Typography>Autocomplete</Typography>;
+  return <Typography>Autocomplete tes</Typography>;
 }

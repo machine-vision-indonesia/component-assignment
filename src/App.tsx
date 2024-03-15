6740d8976e0b836409271529f616159263550b06
Refsi Gregory <refsisangkay@gmail.com>
2024-03-15 15:06:49 +0800

    add MUI component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Autocomplete } from "./components/Autocomplete";
+import Autocomplete from "./components/Autocomplete";
 import { queryClient } from "./utils";
 
 function App() {
+  const fetchOptions = async () => {
+    return [
+      {
+        label: "asd",
+        value: "asd"
+      }
+    ];
+  };
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Autocomplete />
+      <Autocomplete
+        label="Test"
+        options={fetchOptions}
+        />
     </QueryClientProvider>
   );
 }
